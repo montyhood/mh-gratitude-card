9cdf27ceafcac056fa3708e41a51c9e1b147a880
Monty Hood <montyhood@gmail.com>
2021-11-23 00:18:09 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import PropTypes from "prop-types";
 import './App.css';
-import Shapes from './components/shapes/shapes'
-import Logos from './components/logos/logos'
-import {ReactComponent as MakeLogo} from './logo.svg'
-import {ReactComponent as ReactLogo} from './react-logo.svg'
 
-function App({ headline, showLogos, backgroundImage }) {
+function App({ headline, backgroundImage }) {
   return (
     <div className="App" style={{ backgroundImage: `url('${backgroundImage}')` }}>
-      {showLogos && (
-        <Logos logoOne={<MakeLogo />} logoTwo={<ReactLogo />} />
-      )}
 
       <h1>{headline}</h1>
 
@@ -20,13 +13,11 @@ function App({ headline, showLogos, backgroundImage }) {
 
 App.propTypes = {
   headline: PropTypes.string,
-  showLogos: PropTypes.string,
   backgroundImage: PropTypes.string,
 }
 
 App.defaultProps = {
   headline: '',
-  showLogos: false,
   backgroundImage: '',
 }
 
